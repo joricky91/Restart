97805869619f0812d28ac0f5a4be34a3f854fe76
Jonathan Ricky <jonathan.ricky@noble-software.com>
2023-11-27 09:37:00 +0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jonathan Ricky on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
