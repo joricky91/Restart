a2fb0ecb27d2b09e66dff0d0854fbe86507d7a01
Jonathan Ricky <jonathan.ricky@noble-software.com>
2023-11-28 14:42:50 +0700

    feat: play sound and add haptic feedback

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -17,6 +17,7 @@ struct OnboardingView: View {
     @State private var indicatorOpacity: Double = 1.0
     
     @State private var textTitle: String = "Share."
+    let hapticFeedback = UINotificationFeedbackGenerator()
     
     var body: some View {
         ZStack {
@@ -149,9 +150,12 @@ struct OnboardingView: View {
                                 }
                                 .onEnded { _ in
                                     if buttonOffset > buttonWidth / 2 {
+                                        hapticFeedback.notificationOccurred(.success)
+                                        playSound(sound: "chimeup", type: "mp3")
                                         buttonOffset = buttonWidth - 80
                                         isOnboardingViewActive = false
                                     } else {
+                                        hapticFeedback.notificationOccurred(.warning)
                                         buttonOffset = 0
                                     }
                                 }
@@ -170,6 +174,7 @@ struct OnboardingView: View {
         .onAppear {
             isAnimating = true
         }
+        .preferredColorScheme(.dark)
     }
 }
 
